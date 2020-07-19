c5e2758228d8671e1814f1d5d461939686b7ef2e
Marijana Satler <marijanasatler@gmail.com>
2020-07-19 21:13:39 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+        ELECTRONIC BUSINESS CARD
+      
         </a>
       </header>
     </div>
